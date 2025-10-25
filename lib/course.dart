b2fb62ea7778237a35df59344c555a8592034f95
ABO0069 <ayush.biswas@g.bracu.ac.bd>
2025-10-25 18:44:24 +0600

    module 10 assign done

---
+++ lib/course.dart
@@ -0,0 +1,47 @@
+
+class Course {
+  final String imageUrl;
+  final String title;
+  final String batchno;
+  final String seatCount;
+  final String daysLeft;
+
+  Course({
+    required this.imageUrl,
+    required this.title,
+    required this.batchno,
+    required this.seatCount,
+    required this.daysLeft,
+  });
+}
+
+List<Course> courses = [
+  Course(
+    imageUrl: 'assets/mern.jpg', 
+    title: 'Full Stack Web Development with JavaScript (MERN)',
+    batchno: '১৪',
+    seatCount: '৬৯ সিট বাকী',
+    daysLeft: '৩৬ দিন বাকি',
+  ),
+  Course(
+    imageUrl: 'assets/pythonreact.jpg', 
+    title: 'Full Stack Web Development with Python, Django & React',
+    batchno: '৮',
+    seatCount: '৭ সিট বাকী',
+    daysLeft: '১ দিন বাকি',
+  ),
+  Course(
+    imageUrl: 'assets/asp.jpg', 
+    title: 'Full Stack Web Development with ASP.NET Core',
+    batchno: '১০',
+    seatCount: '৩৪ সিট বাকী',
+    daysLeft: '২০ দিন বাকি',
+  ),
+  Course(
+    imageUrl: 'assets/sqa.jpg', 
+    title: 'SQA: Manual & Automated Testing',
+    batchno: '৬',
+    seatCount: '১২ সিট বাকী',
+    daysLeft: '১৫ দিন বাকি',
+  ),
+];
---
+++ lib/course_card.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:course_card_ui/course.dart';
+
+class CourseCard extends StatelessWidget {
+  final Course course;
+
+  const CourseCard({
+    Key? key,
+    required this.course,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      clipBehavior: Clip.antiAlias, 
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            flex: 10, 
+            child: Image.asset(
+              course.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 6.0),
+            child: Wrap(
+              spacing : 8.0,
+              runSpacing: 4.0,
+              //mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                _buildBadgeItem(null, "ব্যাচ ${course.batchno}"),
+                const SizedBox(width: 8),
+                _buildBadgeItem(Icons.people, course.seatCount),
+                const SizedBox(width: 8),
+                _buildBadgeItem(Icons.access_time, course.daysLeft),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+            child: Text(
+              course.title,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+
+          const Spacer(flex: 3), 
+
+          Container(
+            margin: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
+            decoration: BoxDecoration(
+              color: Colors.grey[100],
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'বিস্তারিত দেখুন',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(width: 8),
+                Icon(Icons.arrow_forward, size: 18, color: Colors.black),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildBadgeItem(IconData? icon, String text) {
+    Widget content;
+    if (icon == null) {
+      content = Text(
+        text,
+        style: TextStyle(fontSize: 12, color: Colors.grey[800]),
+      );
+    } else {
+      content = Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(
+            icon,
+            size: 14,
+            color: Colors.grey[700],
+          ),
+          const SizedBox(width: 4),
+          Text(
+            text,
+            style: TextStyle(fontSize: 12, color: Colors.grey[800]),
+          ),
+        ],
+      );
+    }
+
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      decoration: BoxDecoration(
+        color: Colors.grey[200],
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: content,
+    );
+  }
+}
